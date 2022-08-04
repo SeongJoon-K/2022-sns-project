e6a23497e61d306b2d0041478571cab3e68023d6
SEONGJOON KIM <jobcho6320@naver.com>
2022-08-04 23:54:20 +0900

    like 모델 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from venv import create
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -28,3 +29,11 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        Unique_together = (('user', 'post'))
